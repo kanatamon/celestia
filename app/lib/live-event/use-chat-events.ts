bb2cc39e10b9389f3d6057a41b00a641fe4ddd8b
Kantapon Sritiyot <o_kantapon.s@kbtg.tech>
2025-06-18 17:14:06 +0700

    fix: use nickname instead of uniqueId for user mention detection

--- app/lib/live-event/use-chat-events.ts
+++ app/lib/live-event/use-chat-events.ts
@@ -2,13 +2,13 @@ import { useLiveEventStore } from './live-event-store';
 
 export const useUserRelatedChatEvents = (userId: string) => {
 	const chatEvents = useLiveEventStore((state) => state.chatEvents);
-	const viewerUsername = chatEvents.find(
+	const viewerName = chatEvents.find(
 		(event) => event.userId === userId,
-	)?.uniqueId;
+	)?.nickname;
 	return chatEvents.filter((event) => {
-		if (event.type === 'chat' && viewerUsername) {
+		if (event.type === 'chat' && viewerName) {
 			return (
-				event.comment.includes(`@${viewerUsername}`) || event.userId === userId
+				event.comment.includes(`@${viewerName}`) || event.userId === userId
 			);
 		}
 		return event.userId === userId;
