e1ab8dadc4e618d27e68836ae498ee6251f38d08
Kantapon Sritiyot <o_kantapon.s@kbtg.tech>
2025-06-25 22:38:44 +0700

    fix: add overflow hidden style to LiveLayout component

--- app/routes/_live.feed.$username.tsx
+++ app/routes/_live.feed.$username.tsx
@@ -34,6 +34,7 @@ export default function LiveLayout({
 				backgroundPosition: 'center',
 				width: '100%',
 				height: '100%',
+				overflow: 'hidden',
 			}}
 		>
 			{connection.status === 'connecting' ? (
